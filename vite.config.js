5f9c0a1d96bf40b2b2cd4e8852279186cd34614e
Alexsander-fabricio <elesmano8@gmail.com>
2026-07-03 22:34:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: process.cwd(),
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  server: {
+    middlewareMode: false,
+  },
 });
